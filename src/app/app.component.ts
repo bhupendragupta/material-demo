84368b0a874bb34519f271fbb693d770fa9651e2
Bhupendra Gupta <bhupendra1086@gmail.com>
2021-04-21 22:42:01 +0530

    Angular Material tutorial from the free code camp youtube video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'material-demo';
+  title = 'Material Demo';
+
+  logChange(index) {
+    console.log(index);
+  }
 }
